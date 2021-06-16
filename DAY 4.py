a5181d0e4c0f22de4bd154c9775026a4bbd519c7
deepika7007 <85818729+deepika7007@users.noreply.github.com>
2021-06-16 08:40:51 +0530

    Update and rename README.md to DAY 4.py

---
+++ DAY 4.py
@@ -0,0 +1,24 @@
+#DAY 4 phython variables and Data types
+
+# Addition ,Multiply and Division
+
+a=b=c=500
+print("The value of a=b=c is ",c)
+print("Adding the value of a with 30 ",a+30)
+print("Multiply the value of b by 50 ",b*50)
+print("Dividing the value of c by 10 ",c/10)
+
+#Creating a String Variable and Replacing the characters
+
+Variable = "Sweet"
+print("String Variable is ",Variable)
+V1=Variable.replace("e","g")
+print("The Replaced String is ",V1)
+
+a,b=7,3.5
+print("The Float value of a is ",float(a))
+print("The int value of b is ",int(b))
+a1=b
+a2=a
+print("The covereted int value of a is ",int(a2))
+print("The converted Float value of b is ",float(a1))
