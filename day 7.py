1cca91192a03d1bd24f34dbdabb12787c446f0a2
deepika7007 <85818729+deepika7007@users.noreply.github.com>
2021-06-19 07:12:39 +0530

    Create day 7.py

---
+++ day 7.py
@@ -0,0 +1,21 @@
+# day 7
+
+# Create a function getting two integers inputs from user
+a=int(input("Enter the value of first integer a = "))
+b=int(input("Enter the value of first integer b = "))
+def values(a,b):
+    p=a+b
+    q=a-b
+    r=a*b
+    s=a/b
+    print("Addition of two integer is ",p)
+    print("Subtraction of two integer is ",q)
+    print("Multiplication of two integers is ",r)
+    print("Division of two integers is ",s)
+values(a,b)
+
+def covid(r):
+    print("Patient Name =", patientname)
+    print("Body temperature = 98 degree")
+patientname = input("Enter the patient name : ")
+covid(patientname)
