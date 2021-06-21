62d55ff07406f3fbc5ba943dd99a3c5dd9a9a543
deepika7007 <85818729+deepika7007@users.noreply.github.com>
2021-06-21 09:08:07 +0530

    Update day 8.py

--- day 8.py
+++ day 8.py
@@ -9,3 +9,13 @@ x = {'oraNGE': 1 , "white" : 2 , "blue"  : 3 }
 y = {"Pink" : 4 , "green" : 5 , "lavender" : 6 }  
 z = dict(x,y)
 print("The Merged dictionaries are ",z)
+
+#program to sort the value from descending to ascending in list and convert it in to a set.
+A=[1,3,5,7,15,9,8,13,16]
+A.sort(reverse=True)
+print("The Value in descending order is ",A )
+A.sort()
+print("The Value in Ascending order is ",A )
+
+S_1 = set(A)
+print("The set value is ",S_1)
